360d3d07b3946a3950e8eaf090123ae7ccf62e70
Santics2324 <141960472+Santics2324@users.noreply.github.com>
2025-03-05 09:28:33 -0500

    Add files via upload

---
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/repositories/PacienteRepository.java
@@ -0,0 +1,10 @@
+package co.edu.uniandes.dse.parcialprueba.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniandes.dse.parcialprueba.entities.PacienteEntity;
+
+public interface PacienteRepository extends JpaRepository<PacienteEntity, Long> 
+{
+
+} 
